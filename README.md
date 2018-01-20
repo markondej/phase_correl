cdb63e82382fd76a0926e21aa458acbc22547d9e
Marcin Kondej <markondej@gmail.com>
2018-01-20 01:40:41 +0100

    Updated code and markdown

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # phase_correl
-Simple phase correlation algorithm written in C
+
+This simple implementation represents phase correlation based image translation computing in action. Though it has not been optimized for speed, this algorithm is good base for real time image alignment. I used it as a base for my image processing researches in 2010 while working at Warsaw Univeristy of Technology.
+
+Phase correlation feature matching was described by Yan H. and Liu J.G. in article "Roboust phase correlation based feature matching for image co-registration and DEM generation" (http://www.isprs.org/proceedings/XXXVII/congress/7_pdf/6_WG-VII-6/43.pdf).
+
+## How it works
+
+At first two monochromatic images are created, both containing squares of the same size but in different places. 
+![Squares](squares.png)
+Next, 2D Fourier transform is applied on each of them, normalised cross power spectrum is computed, and inverted 2D Fourier transform is applied. Finally algorightm searches for a peak in obtained matrix, where address of peak found should match shift between images.
+
+## How to compile
+
+Simply clone this repository on linux machine by typing:
+```
+git clone https://github.com/markondej/phase_correlation.git
+```
+Then build executable binary with "make" command:
+```
+make
+```
+ 
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 unsigned char image1[256*128];
@@ -8,39 +9,39 @@ void ditfft2(double *input, double *output, int size, int s, int inv) {
     double temp[4], fi, kfi, cosfi, sinfi;
     int k, k2;
 
-	if (size > 1) {
-		ditfft2(&input[0], &output[0], size/2, s*2, inv);
-		ditfft2(&input[s*2], &output[size], size/2, s*2, inv);
+    if (size > 1) {
+        ditfft2(&input[0], &output[0], size/2, s*2, inv);
+        ditfft2(&input[s*2], &output[size], size/2, s*2, inv);
 
         if (!inv) {
             fi = -2.0*M_PI/(double)size; kfi = 0.0;
         } else {
             fi = 2.0*M_PI/(double)size; kfi = 0.0;
         }
-		for (k = 0; k < size/2; k++) {
-			cosfi = cos(kfi); sinfi = sin(kfi); k2 = k*2;
-			temp[0] = output[k2]; temp[1] = output[k2+1];
-			temp[2] = output[size+k2]; temp[3] = output[size+k2+1];
+        for (k = 0; k < size/2; k++) {
+            cosfi = cos(kfi); sinfi = sin(kfi); k2 = k*2;
+            temp[0] = output[k2]; temp[1] = output[k2+1];
+            temp[2] = output[size+k2]; temp[3] = output[size+k2+1];
 
-			output[k2] = temp[0]+temp[2]*cosfi-temp[3]*sinfi;
-			output[k2+1] = temp[1]+temp[2]*sinfi+temp[3]*cosfi;
-			output[size+k2] = temp[0]-temp[2]*cosfi+temp[3]*sinfi;
-			output[size+k2+1] = temp[1]-temp[2]*sinfi-temp[3]*cosfi;
-			kfi += fi;
-		}
-	}
-	else {
-		output[0] = input[0];
-		output[1] = input[1];
-	}
+            output[k2] = temp[0]+temp[2]*cosfi-temp[3]*sinfi;
+            output[k2+1] = temp[1]+temp[2]*sinfi+temp[3]*cosfi;
+            output[size+k2] = temp[0]-temp[2]*cosfi+temp[3]*sinfi;
+            output[size+k2+1] = temp[1]-temp[2]*sinfi-temp[3]*cosfi;
+            kfi += fi;
+        }
+    }
+    else {
+        output[0] = input[0];
+        output[1] = input[1];
+    }
 }
 
 void fft(double *input, double *output, int size) {
-	ditfft2(input, output, size, 1, 0);
+    ditfft2(input, output, size, 1, 0);
 }
 
 void ifft(double *input, double *output, int size) {
-	ditfft2(input, output, size, 1, 1);
+    ditfft2(input, output, size, 1, 1);
 }
 
 void fft2D(double *input, int width, int height) {
@@ -50,32 +51,32 @@ void fft2D(double *input, int width, int height) {
     double *fft_output = (double *)malloc(sizeof(double)*width*2);
     double *fft_temp = (double *)malloc(sizeof(double)*width*height*2);
 
-	for (j = 0; j < height; j++) {
-		for (i = 0; i < width; i++) {
-			fft_input[i*2] = input[(i+j*width)*2];
-			fft_input[i*2+1] = input[(i+j*width)*2+1];
-		}
-		fft(fft_input, fft_output, width);
-		for (i = 0; i < width; i++) {
-			fft_temp[(i+j*width)*2] = fft_output[i*2];
-			fft_temp[(i+j*width)*2+1] = fft_output[i*2+1];
-		}
-	}
-	for (i = 0; i < width; i++) {
-		for (j = 0; j < height; j++) {
-			fft_input[j*2] = fft_temp[(i+j*width)*2];
-			fft_input[j*2+1] = fft_temp[(i+j*width)*2+1];
-		}
-		fft(fft_input, fft_output, height);
-		for (j = 0; j < height; j++) {
-			input[(i+j*width)*2] = fft_output[j*2];
-			input[(i+j*width)*2+1] = fft_output[j*2+1];
-		}
-	}
+    for (j = 0; j < height; j++) {
+        for (i = 0; i < width; i++) {
+            fft_input[i*2] = input[(i+j*width)*2];
+            fft_input[i*2+1] = input[(i+j*width)*2+1];
+        }
+        fft(fft_input, fft_output, width);
+        for (i = 0; i < width; i++) {
+            fft_temp[(i+j*width)*2] = fft_output[i*2];
+            fft_temp[(i+j*width)*2+1] = fft_output[i*2+1];
+        }
+    }
+    for (i = 0; i < width; i++) {
+        for (j = 0; j < height; j++) {
+            fft_input[j*2] = fft_temp[(i+j*width)*2];
+            fft_input[j*2+1] = fft_temp[(i+j*width)*2+1];
+        }
+        fft(fft_input, fft_output, height);
+        for (j = 0; j < height; j++) {
+            input[(i+j*width)*2] = fft_output[j*2];
+            input[(i+j*width)*2+1] = fft_output[j*2+1];
+        }
+    }
 
-	free(fft_input);
-	free(fft_output);
-	free(fft_temp);
+    free(fft_input);
+    free(fft_output);
+    free(fft_temp);
 }
 
 void ifft2D(double *input, int width, int height) {
@@ -85,38 +86,39 @@ void ifft2D(double *input, int width, int height) {
     double *fft_output = (double *)malloc(sizeof(double)*width*2);
     double *fft_temp = (double *)malloc(sizeof(double)*width*height*2);
 
-	for (i = 0; i < width; i++) {
-		for (j = 0; j < height; j++) {
-			fft_input[j*2] = input[(i+j*width)*2];
-			fft_input[j*2+1] = input[(i+j*width)*2+1];
-		}
-		ifft(fft_input, fft_output, height);
-		for (j = 0; j < height; j++) {
-			fft_temp[(i+j*width)*2] = fft_output[j*2]/(double)height;
-			fft_temp[(i+j*width)*2+1] = fft_output[j*2+1]/(double)height;
-		}
+    for (i = 0; i < width; i++) {
+        for (j = 0; j < height; j++) {
+            fft_input[j*2] = input[(i+j*width)*2];
+            fft_input[j*2+1] = input[(i+j*width)*2+1];
+        }
+        ifft(fft_input, fft_output, height);
+        for (j = 0; j < height; j++) {
+            fft_temp[(i+j*width)*2] = fft_output[j*2]/(double)height;
+            fft_temp[(i+j*width)*2+1] = fft_output[j*2+1]/(double)height;
+        }
+    }
+    for (j = 0; j < height; j++) {
+        for (i = 0; i < width; i++) {
+            fft_input[i*2] = fft_temp[(i+j*width)*2];
+            fft_input[i*2+1] = fft_temp[(i+j*width)*2+1];
 	}
-	for (j = 0; j < height; j++) {
-		for (i = 0; i < width; i++) {
-			fft_input[i*2] = fft_temp[(i+j*width)*2];
-			fft_input[i*2+1] = fft_temp[(i+j*width)*2+1];
-		}
-		ifft(fft_input, fft_output, width);
-		for (i = 0; i < width; i++) {
-			input[(i+j*width)*2] = fft_output[i*2]/(double)width;
-			input[(i+j*width)*2+1] = fft_output[i*2+1]/(double)width;
-		}
-	}
+	ifft(fft_input, fft_output, width);
+        for (i = 0; i < width; i++) {
+            input[(i+j*width)*2] = fft_output[i*2]/(double)width;
+            input[(i+j*width)*2+1] = fft_output[i*2+1]/(double)width;
+        }
+    }
 
-	free(fft_input);
-	free(fft_output);
-	free(fft_temp);
+    free(fft_input);
+    free(fft_output);
+    free(fft_temp);
 }
 
 int main()
 {
     int i, j, tx, ty;
 
+    // Generate pair of images
     for (j = 0; j < 128; j++)
         for (i = 0; i < 256; i++) {
             if ((i > 16) && (i < 80) && (j > 32) && (j < 96))
@@ -144,18 +146,20 @@ int main()
         }
     }
 
+    // Perform 2D FFT on each image
     fft2D(fft_input1, 256, 128);
     fft2D(fft_input2, 256, 128);
 
+    // Compute nomalised cross power spectrum
     for (j = 0; j < 128; j++)
         for (i = 0; i < 256; i++) {
             double a = fft_input1[(i+j*256)*2];
             double b = fft_input1[(i+j*256)*2+1];
             double c = fft_input2[(i+j*256)*2];
             double d = fft_input2[(i+j*256)*2+1];
-            
+
             double a1, a2;
-            
+
             if (b != 0.0) {
             	if (a != 0.0) {
                     a1 = atan(fabs(b/a));
@@ -163,44 +167,46 @@ int main()
                     a1 = M_PI/2.0;
                 }
             } else a1 = 0.0;
-            
+
             if ((a < 0.0) && (b > 0.0)) a1 = M_PI-a1;
             if ((a < 0.0) && (b < 0.0)) a1 = M_PI+a1;
             if ((a > 0.0) && (b < 0.0)) a1 = 2.0*M_PI-a1;
-        
+
             if (d != 0.0) {
                 if (c != 0.0) {
                     a2 = atan(fabs(d/c));
                 } else {
                     a2 = M_PI/2.0;
                 }
-            } else a2 = 0.0;        
-        
+            } else a2 = 0.0;
+
             if ((c < 0.0) && (d > 0.0)) a2 = M_PI-a2;
             if ((c < 0.0) && (d < 0.0)) a2 = M_PI+a2;
             if ((c > 0.0) && (d < 0.0)) a2 = 2.0*M_PI-a2;
-            
+
             fft_output[(i+j*256)*2] = cos(a1-a2);
             fft_output[(i+j*256)*2+1] = sin(a1-a2);
         }
 
+    // Perform inversed 2D FFT on obtained matrix
     ifft2D(fft_output, 256, 128);
 
-	double max = 0.0; tx = 0; ty = 0;
-	for (j = 0; j < 128; j++)
-		for (i = 0; i < 256; i++) {
-			double d = sqrt(pow(fft_output[(i+j*256)*2], 2)+pow(fft_output[(i+j*256)*2+1], 2));
-			if (d > max) {
-				max = d; tx = i; ty = j;
-			}
-		}
+    // Search for peak
+    double max = 0.0; tx = 0; ty = 0;
+    for (j = 0; j < 128; j++)
+        for (i = 0; i < 256; i++) {
+            double d = sqrt(pow(fft_output[(i+j*256)*2], 2)+pow(fft_output[(i+j*256)*2+1], 2));
+            if (d > max) {
+                max = d; tx = i; ty = j;
+            }
+        }
 
     if (tx > 256/2)
         tx = tx-256;
     if (ty > 128/2)
         ty = ty-128;
 
-    printf("Translation: (%d, %d)!\n", tx, ty);
+    printf("Computed translation: [%d, %d]\n", tx, ty);
 
     free(fft_input1);
     free(fft_input2);
